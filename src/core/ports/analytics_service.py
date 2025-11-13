d3aeaaf04cf5d11980ea42c056e208715c656cd3
Carlos Sandoval <csandovalc@unal.edu.co>
2025-11-12 19:04:58 -0500

    fix: resolve type reference
    Signed-off-by: Carlos Sandoval <csandovalc@unal.edu.co>

--- src/core/ports/analytics_service.py
+++ src/core/ports/analytics_service.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import List, Optional
+from typing import List, Optional, Dict, Any
 from datetime import datetime
 
 from ..domain.measurement import Measurement
